9a812b3c259677d95007827480fa9b7b089d3d37
oresoftware <alex@oresoftware.com>
2016-11-22 01:13:02 -0800

    init

---
+++ suman-installation-test-project-custom-config/test/one.test.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Olegzandr on 11/20/16.
+ */
+
+
+const suman = require('suman');
+const Test = suman.init(module, {
+  ioc: {}
+});
+
+Test.create('makes birds fly', function (it, describe) {
+
+   describe('magic', function(it){
+
+     it('is great', function(){
+
+     });
+
+     it('is great', function(){
+
+     });
+
+     it('is great', function(){
+
+     });
+
+
+   });
+
+
+});
---
+++ suman-installation-test-project/test/one.test.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Olegzandr on 11/20/16.
+ */
+
+
+const suman = require('suman');
+const Test = suman.init(module);
+
+Test.create('makes birds fly', function (it, describe) {
+
+   describe('magic', function(it){
+
+     it('is great', function(){
+
+     });
+
+     it('is great', function(){
+
+     });
+
+     it('is great', function(){
+
+     });
+
+
+   });
+
+
+});
