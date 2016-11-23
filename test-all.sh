7887ebb986e822ae0d690e06cd2ce752739d00b4
oresoftware <alex@oresoftware.com>
2016-11-22 23:47:57 -0800

    temp

--- test-all.sh
+++ test-all.sh
@@ -3,5 +3,5 @@
 # this script will cd into each directory and run the bash script called test.sh in each directory
 # if this script exits cleanly, then all tests pass
 
-npm install -D suman &&
+npm install -D oresoftware/suman &&  # installs via github, not npm
 ./node_modules/.bin/suman test-all.js --cwd-is-root
