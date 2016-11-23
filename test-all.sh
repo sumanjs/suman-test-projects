99500b540ae5e1babcf1112082ae16f97e6c1850
oresoftware <alex@oresoftware.com>
2016-11-23 00:11:10 -0800

    whoa

--- test-all.sh
+++ test-all.sh
@@ -3,5 +3,6 @@
 # this script will cd into each directory and run the bash script called test.sh in each directory
 # if this script exits cleanly, then all tests pass
 
+echo "jesus is watching" &&
 npm install -D oresoftware/suman &&  # installs via github, not npm
 ./node_modules/.bin/suman test-all.js --cwd-is-root
