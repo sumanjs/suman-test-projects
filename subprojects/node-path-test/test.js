306e6fe2f7681570e2b8e607f8719e7b54ff1c2d
Olegzandr Denman <alex@oresoftware.com>
2016-12-04 21:13:41 -0800

    mast

---
+++ subprojects/node-path-test/test.js
@@ -0,0 +1,11 @@
+/**
+ * Created by olegzandr on 12/4/16.
+ */
+
+
+// const which = require('which');
+// var resolved = which.sync('istanbul');
+//
+// console.log(resolved);
+
+console.log(require.resolve('istanbul'));
---
+++ subprojects/node-path-test/test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+
+#npm --loglevel=warn --progress=false install &&
+NODE_PATH=${NODE_PATH}:~/.suman/node_modules && node test.js
+#node test.js
