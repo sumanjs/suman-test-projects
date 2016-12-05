8db124992eec5a175646296886666627cc376ed5
Olegzandr Denman <alex@oresoftware.com>
2016-12-04 22:35:34 -0800

    temp

--- subprojects/node-path-test/test.sh
+++ subprojects/node-path-test/test.sh
@@ -4,6 +4,7 @@
 
 OUTPUT_PATH=${PROJECT_ROOT}/npm-install-output.log
 
+
 npm --loglevel=warn --progress=false install ${OUTPUT_PATH} 2>&1 &&
 NODE_PATH=${NODE_PATH}:~/.suman/node_modules && node test.js
 #node test.js
